f17da58db0404fddd122da7e4a658f8e3e994316
Nicolas Pinto <nicolas.pinto@gmail.com>
2025-04-15 18:43:28 -0700

    Fix a small typo in HwndWrapper.py

--- pywinauto/controls/HwndWrapper.py
+++ pywinauto/controls/HwndWrapper.py
@@ -71,7 +71,7 @@ class InvalidWindowHandle(RuntimeError):
     def __init__(self, hwnd):
         "Initialise the RuntimError parent with the mesage"
         RuntimeError.__init__(self,
-            "Handle 0x%d is not a vaild window handle"% hwnd)
+            "Handle 0x%d is not a valid window handle"% hwnd)
 
 
 
